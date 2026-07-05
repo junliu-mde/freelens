f7d6a1640ac00d88077a3b692465b32e5df409d3
Jun Liu <jun.c.liu@rakuten.com>
2026-07-05 16:18:14 +0900

    feat(sidebar): add LeaderWorkerSets and RoleBasedGroups sidebar items under workloads menu

---
+++ packages/core/src/renderer/components/workloads-leaderworkerset/sidebar-item.injectable.tsx
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Freelens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import { sidebarItemInjectionToken } from "@freelensapp/cluster-sidebar";
+import { getInjectable } from "@ogre-tools/injectable";
+import { computed } from "mobx";
+import { matchPath } from "react-router-dom";
+import customResourcesRouteInjectable from "../../../common/front-end-routing/routes/cluster/custom-resources/custom-resources-route.injectable";
+import navigateToCustomResourcesInjectable from "../../../common/front-end-routing/routes/cluster/custom-resources/navigate-to-custom-resources.injectable";
+import currentPathInjectable from "../../routes/current-path.injectable";
+import workloadsSidebarItemInjectable from "../workloads/workloads-sidebar-item.injectable";
+
+import type { CustomResourcesPathParameters } from "../../../common/front-end-routing/routes/cluster/custom-resources/custom-resources-route.injectable";
+
+const leaderWorkerSetSidebarItemInjectable = getInjectable({
+  id: "sidebar-item-leaderworkerset",
+
+  instantiate: (di) => {
+    const navigateToCustomResources = di.inject(navigateToCustomResourcesInjectable);
+    const currentPath = di.inject(currentPathInjectable);
+    const route = di.inject(customResourcesRouteInjectable);
+
+    return {
+      parentId: workloadsSidebarItemInjectable.id,
+      title: "LeaderWorkerSets",
+      onClick: () =>
+        navigateToCustomResources({
+          group: "leaderworkerset.x-k8s.io",
+          name: "leaderworkersets",
+        }),
+      isActive: computed(() => {
+        const match = matchPath<CustomResourcesPathParameters>(currentPath.get(), {
+          path: route.path,
+          exact: true,
+        });
+
+        return match?.params.group === "leaderworkerset.x-k8s.io" && match?.params.name === "leaderworkersets";
+      }),
+      isVisible: computed(() => true),
+      orderNumber: 52,
+    };
+  },
+
+  injectionToken: sidebarItemInjectionToken,
+});
+
+export default leaderWorkerSetSidebarItemInjectable;
