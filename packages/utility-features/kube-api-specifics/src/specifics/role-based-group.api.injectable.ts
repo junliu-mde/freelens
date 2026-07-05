7f8880d6e7d1c9c43c9ca5fbb1fe369668c3d96f
Jun Liu <jun.c.liu@rakuten.com>
2026-07-05 16:01:59 +0900

    fix(api): register RoleInstance and RoleBasedGroup KubeApis to correctly construct sub-class models in UI

---
+++ packages/utility-features/kube-api-specifics/src/specifics/role-based-group.api.injectable.ts
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) Freelens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import { RoleBasedGroupApi } from "@freelensapp/kube-api";
+import {
+  logDebugInjectionToken,
+  logErrorInjectionToken,
+  logInfoInjectionToken,
+  logWarningInjectionToken,
+} from "@freelensapp/logger";
+import { getInjectable } from "@ogre-tools/injectable";
+import assert from "assert";
+import { storesAndApisCanBeCreatedInjectionToken } from "./can-be-created-token";
+import { maybeKubeApiInjectable } from "./maybe-kube-api.injectable";
+import { kubeApiInjectionToken } from "./token";
+
+export const roleBasedGroupApiInjectable = getInjectable({
+  id: "role-based-group-api",
+  instantiate: (di) => {
+    assert(
+      di.inject(storesAndApisCanBeCreatedInjectionToken),
+      "roleBasedGroupApi is only available in certain environments",
+    );
+
+    return new RoleBasedGroupApi({
+      logDebug: di.inject(logDebugInjectionToken),
+      logError: di.inject(logErrorInjectionToken),
+      logInfo: di.inject(logInfoInjectionToken),
+      logWarn: di.inject(logWarningInjectionToken),
+      maybeKubeApi: di.inject(maybeKubeApiInjectable),
+    });
+  },
+
+  injectionToken: kubeApiInjectionToken,
+});
+
+export default roleBasedGroupApiInjectable;
